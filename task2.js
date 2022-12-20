729eb15b9155b64141320a24ed528ba1df9a66db
user <kupaev.itrum@gmail.com>
2022-12-20 14:00:27 +0300

    done task2

---
+++ task2.js
@@ -0,0 +1,13 @@
+const arr = [14, 53, 23, 53, 13, 55, 7, 34, 3, 6];
+
+function getByEvenIndices(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            result.push(arr[i]);
+        }
+    }
+    return result
+}
+
+console.log(getByEvenIndices(arr))
