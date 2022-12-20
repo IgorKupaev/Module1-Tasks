73f51d68b5cecd9d9b77a6fb3527452f6ebb7f01
user <kupaev.itrum@gmail.com>
2022-12-20 15:16:10 +0300

    fixed task1

--- task1.js
+++ task1.js
@@ -1,9 +1,11 @@
 const list = [{name: 'Milk', price: 20, amount: 15},
             {name: 'Coffe', price: 30, amount: 17},
             { name: 'Tea', price: 10, amount: 14 }]; 
+
 function getTotal(list) {
     return list.map(item => {
         return {name: item.name, total: item.price * item.amount}
     });
 };
+
 console.log(getTotal(list))
