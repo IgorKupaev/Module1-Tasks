f705685ac727b6dcd40c973a5718028326e26548
user <kupaev.itrum@gmail.com>
2022-12-21 10:17:12 +0300

    fixed task16

--- task16.js
+++ task16.js
@@ -1,7 +1,17 @@
-const num = '88005553535';
+const num = '+89282110876';
 
 function formatNumber(n) {
-    return `8 ${n.slice(1, 4)} ${n.slice(4, 7)}-${n.slice(7, 9)}-${n.slice(9, 11)}`;
+    n = n.split("").reverse().join("");
+    let part1 = n.slice(7, 10).split("").reverse().join("");
+    let part2 = n.slice(4, 7).split("").reverse().join("");
+    let part3 = n.slice(2, 4).split("").reverse().join("");
+    let part4 = n.slice(0, 2).split("").reverse().join("");
+    if (n[n.length - 1] === "+") {
+        numCode = n.slice(10, n.length).split("").reverse().join('');
+    } else {
+        numCode = n.slice(10, 11);
+    }
+    return `${numCode} ${part1} ${part2}-${part3}-${part4}`;
 }
 
 console.log(formatNumber(num))
