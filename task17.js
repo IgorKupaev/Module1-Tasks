6763c99eb9c75a0ac4645583ce53a7d45e1063f0
user <kupaev.itrum@gmail.com>
2022-12-21 10:26:25 +0300

    fixed task17

--- task17.js
+++ task17.js
@@ -1,7 +1,11 @@
 let arr = [1, 2, 3];
 
 function concatArrays() {
-    return [].concat(...arguments)
+    let result = [];
+    for (arg of arguments) {
+        result = [...result, ...arg];
+    }
+    return result;
 }
 
-console.log(concatArrays(arr, arr, arr))
+console.log(concatArrays([0, -1, +1], [-7, 12, 44], [2, 3, 5]))
