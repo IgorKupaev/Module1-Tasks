01b34c449cbd4e4866adaafa9c1d8cb949f87fe7
user <kupaev.itrum@gmail.com>
2022-12-21 09:54:37 +0300

    fixed task2

--- task2.js
+++ task2.js
@@ -1,11 +1,7 @@
 const arr = [14, 53, 23, 53, 13, 55, 7, 34, 3, 6];
 
 function getByEvenIndices(arr) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        i % 2 !== 0 || result.push(arr[i]);
-    }
-    return result
+    return arr.filter((el, index) => index % 2 === 0)
 }
 
 console.log(getByEvenIndices(arr));
